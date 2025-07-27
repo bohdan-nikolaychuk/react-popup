464b70071d8924f5adc94e369854b66894d41d43
Bohdan <bohdan.n.upqode@gmail.com>
2025-07-27 21:08:41 +0300

    Refactor vite.config.js to use defineConfig and update entry point to main.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-export default {
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env': {}
+  },
   build: {
     lib: {
-      entry: 'src/main.jsx',
+      entry: 'src/main.js',
       name: 'StepPopupWidget',
       fileName: 'step-popup-widget',
       formats: ['iife'],
     }
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom'], 
   }
-};
+});
